ab31a6475424bce224748a123b9226fadfa210a2
depassage <maisbiensurqueoui@gmail.com>
2017-05-22 11:01:28 +0800

    Prototyping done

--- app/coffescript/index.coffee
+++ app/coffescript/index.coffee
@@ -6,6 +6,14 @@ port = process.argv[2] || 8000
 
 app.use '/static', express.static path.join __dirname, 'public'
 
-
+app.get '/', (req, res) ->
+  ip = (req.headers['x-forwarded-for'] || res.connection.remoteAddress).split(':')
+  ip = ip[ip.length - 1]
+  lang = req.headers['accept-language'].split(',')[0]
+  soft = req.headers['user-agent'].match(/\((.+?)\)/)[1]
+  res.json
+    ipaddress: ip
+    language: lang
+    software: soft
 
 app.listen port
--- app/javascript/index.js
+++ app/javascript/index.js
@@ -12,6 +12,19 @@
 
   app.use('/static', express["static"](path.join(__dirname, 'public')));
 
+  app.get('/', function(req, res) {
+    var ip, lang, soft;
+    ip = (req.headers['x-forwarded-for'] || res.connection.remoteAddress).split(':');
+    ip = ip[ip.length - 1];
+    lang = req.headers['accept-language'].split(',')[0];
+    soft = req.headers['user-agent'].match(/\((.+?)\)/)[1];
+    return res.json({
+      ipaddress: ip,
+      language: lang,
+      software: soft
+    });
+  });
+
   app.listen(port);
 
 }).call(this);
